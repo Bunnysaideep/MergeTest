8900318249775e29684a879908cc92d63776f134
Saideep Sriramoju <saideep.sriramoju@sap.com>
2025-08-16 05:25:00 +0000

    Initial commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("mergetest.controller.View1", {
+        onInit() {
+        }
+    });
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["mergetest/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
